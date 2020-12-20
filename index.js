482ff5e60ecb890e0b34d04b4b82b12b3bba76f2
Sunny Hirai <thesunny@gmail.com>
2020-12-19 23:26:41 -0800

    Read directory to get all pages

--- index.js
+++ index.js
@@ -4,32 +4,43 @@ import fs from "fs-extra"
 // Create a new PDFDocument
 const pdfDoc = await PDFDocument.create()
 
-// These should be Uint8Arrays or ArrayBuffers
-// This data can be obtained in a number of different ways
-// If your running in a Node environment, you could use fs.readFile()
-// In the browser, you could make a fetch() call and use res.arrayBuffer()
-const firstDonorPdfBytes = await fs.readFile(
-  "./addition/spaceship-math-h-v1.pdf"
+const files = await fs.readdir("./src/addition")
+
+await Promise.all(
+  files.map(async (file) => {
+    const donorPdfBytes = await fs.readFile(`./src/addition/${file}`)
+    const donorPdfDoc = await PDFDocument.load(donorPdfBytes)
+    const [donorPage] = await pdfDoc.copyPages(donorPdfDoc, [0])
+    pdfDoc.addPage(donorPage)
+  })
 )
-const secondDonorPdfBytes = await fs.readFile(
-  "./addition/spaceship-math-h-v2.pdf"
-)
 
-// Load a PDFDocument from each of the existing PDFs
-const firstDonorPdfDoc = await PDFDocument.load(firstDonorPdfBytes)
-const secondDonorPdfDoc = await PDFDocument.load(secondDonorPdfBytes)
+// // These should be Uint8Arrays or ArrayBuffers
+// // This data can be obtained in a number of different ways
+// // If your running in a Node environment, you could use fs.readFile()
+// // In the browser, you could make a fetch() call and use res.arrayBuffer()
+// const firstDonorPdfBytes = await fs.readFile(
+//   "./addition/spaceship-math-h-v1.pdf"
+// )
+// const secondDonorPdfBytes = await fs.readFile(
+//   "./addition/spaceship-math-h-v2.pdf"
+// )
 
-// Copy the 1st page from the first donor document, and
-// the 743rd page from the second donor document
-const [firstDonorPage] = await pdfDoc.copyPages(firstDonorPdfDoc, [0])
-const [secondDonorPage] = await pdfDoc.copyPages(secondDonorPdfDoc, [0])
+// // Load a PDFDocument from each of the existing PDFs
+// const firstDonorPdfDoc = await PDFDocument.load(firstDonorPdfBytes)
+// const secondDonorPdfDoc = await PDFDocument.load(secondDonorPdfBytes)
 
-// Add the first copied page
-pdfDoc.addPage(firstDonorPage)
+// // Copy the 1st page from the first donor document, and
+// // the 743rd page from the second donor document
+// const [firstDonorPage] = await pdfDoc.copyPages(firstDonorPdfDoc, [0])
+// const [secondDonorPage] = await pdfDoc.copyPages(secondDonorPdfDoc, [0])
 
-// Insert the second copied page to index 0, so it will be the
-// first page in `pdfDoc`
-pdfDoc.addPage(secondDonorPage)
+// // Add the first copied page
+// pdfDoc.addPage(firstDonorPage)
+
+// // Insert the second copied page to index 0, so it will be the
+// // first page in `pdfDoc`
+// pdfDoc.addPage(secondDonorPage)
 
 // Serialize the PDFDocument to bytes (a Uint8Array)
 const pdfBytes = await pdfDoc.save()
