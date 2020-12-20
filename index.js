ad6281b1b315d46636ae08a4b13bf62c42442410
Sunny Hirai <thesunny@gmail.com>
2020-12-19 23:39:34 -0800

    Allows consuming any number of subdirectories in the src directory. Updating readme.md to match

--- index.js
+++ index.js
@@ -1,48 +1,33 @@
 import { PDFDocument } from "pdf-lib"
 import fs from "fs-extra"
 
-// Create a new PDFDocument
-const pdfDoc = await PDFDocument.create()
+const rootDir = "./src"
 
-const files = await fs.readdir("./src/addition")
+const dirs = await fs.readdir(rootDir)
+
+console.log(dirs)
 
 await Promise.all(
-  files.map(async (file) => {
-    const donorPdfBytes = await fs.readFile(`./src/addition/${file}`)
-    const donorPdfDoc = await PDFDocument.load(donorPdfBytes)
-    const [donorPage] = await pdfDoc.copyPages(donorPdfDoc, [0])
-    pdfDoc.addPage(donorPage)
+  dirs.map(async (subdir) => {
+    // Create a new PDFDocument
+    const pdfDoc = await PDFDocument.create()
+
+    const dir = `${rootDir}/${subdir}`
+    const files = await fs.readdir(dir)
+
+    console.log(`Processing ${dir}`)
+
+    await Promise.all(
+      files.map(async (file) => {
+        const donorPdfBytes = await fs.readFile(`${dir}/${file}`)
+        const donorPdfDoc = await PDFDocument.load(donorPdfBytes)
+        const [donorPage] = await pdfDoc.copyPages(donorPdfDoc, [0])
+        pdfDoc.addPage(donorPage)
+      })
+    )
+
+    const pdfBytes = await pdfDoc.save()
+
+    await fs.outputFile(`./output/${subdir}.pdf`, pdfBytes)
   })
 )
-
-// // These should be Uint8Arrays or ArrayBuffers
-// // This data can be obtained in a number of different ways
-// // If your running in a Node environment, you could use fs.readFile()
-// // In the browser, you could make a fetch() call and use res.arrayBuffer()
-// const firstDonorPdfBytes = await fs.readFile(
-//   "./addition/spaceship-math-h-v1.pdf"
-// )
-// const secondDonorPdfBytes = await fs.readFile(
-//   "./addition/spaceship-math-h-v2.pdf"
-// )
-
-// // Load a PDFDocument from each of the existing PDFs
-// const firstDonorPdfDoc = await PDFDocument.load(firstDonorPdfBytes)
-// const secondDonorPdfDoc = await PDFDocument.load(secondDonorPdfBytes)
-
-// // Copy the 1st page from the first donor document, and
-// // the 743rd page from the second donor document
-// const [firstDonorPage] = await pdfDoc.copyPages(firstDonorPdfDoc, [0])
-// const [secondDonorPage] = await pdfDoc.copyPages(secondDonorPdfDoc, [0])
-
-// // Add the first copied page
-// pdfDoc.addPage(firstDonorPage)
-
-// // Insert the second copied page to index 0, so it will be the
-// // first page in `pdfDoc`
-// pdfDoc.addPage(secondDonorPage)
-
-// Serialize the PDFDocument to bytes (a Uint8Array)
-const pdfBytes = await pdfDoc.save()
-
-await fs.outputFile("./output/addition.pdf", pdfBytes)
--- readme.md
+++ readme.md
@@ -1,18 +1,17 @@
 # Read Me
 
-## How to get started with `next-starter-kit`
+## Getting Started
+
+Place a subdirectory filled with PDF files into the `./src` directory.
+
+Each subdirectory will be converted into one PDF file which will be placed in the `./output` directory.
+
+The PDF file includes only the first page of every file from the subdirectory. In a Dad's Worksheet, that is the question page. The second page is always the answer page.
+
+This makes it easy to print a bunch of question pages in a certain range of questions without having to open and print each page individually.
+
+To run the script enter...
 
 ```sh
-# in your project root, initialize an empty git repo
-git init
-
-# pull next-starter-kit
-git pull git@github.com:thesunny/next-starter-kit.git
-```
-
-## How to update
-
-```sh
-# re-pull next-starter-kit
-git pull git@github.com:thesunny/next-starter-kit.git
+yarn build
 ```
